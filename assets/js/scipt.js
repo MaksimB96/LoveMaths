85efa8ad5949c456c8b711e59a4b28d3c1692a49
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-04 19:57:27 +0000

    Display addition question to the user

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -10,10 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
@@ -22,10 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * I'm also using the doc string in order to describe
  * functions
  */
-function runGame() {
+
+function runGame(gameType) {
+
     //create two random numbers between 1-25
-    let num1 = Math.floor(Math.random()*25)+1;
-    let num2 = Math.floor(Math.random()*25)+1;
+    let num1 = Math.floor(Math.random() * 25)+1;
+    let num2 = Math.floor(Math.random() * 25)+1;
+
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game Type: ${gameType}`);
+        throw `Unknown game Type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -45,7 +56,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
