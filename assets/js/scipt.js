2152c93c4b318a247f67ae4af226c6ae598df6d2
MaksimB96 <maksims.buraks.1996@gmail.com>
2022-12-04 19:12:47 +0000

    Add code to calculate raandom math for the two numbers question

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -1,5 +1,31 @@
-function runGame() {
+//Wait for DOM to finish loading before running the game 
+//Get buttton events and add eveent listeners to them 
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+/**
+ * The main game "loop" called when the script is first loaded
+ * and aafter the users answer has been processed
+ * I'm also using the doc string in order to describe
+ * functions
+ */
+function runGame() {
+    //create two random numbers between 1-25
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random()*25)+1;
 }
 
 function checkAnswer() {
@@ -7,6 +33,7 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    
 
 }
 
@@ -30,6 +57,6 @@ function displayMultiplyQuestion() {
 
 }
 
-function displayDivideQuestion() {
+// function displayDivideQuestion() {
 
-}
+// }
